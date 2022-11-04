dfa6ce5236b34037abb0eec7a57102bace344d74
chizhan9527 <1346885811@qq.com>
2022-11-04 23:10:12 +0800

    跑步总排名的排行榜

--- VideoProject_BackEnd/src/main/java/com/backend/videoproject_backend/service/impl/RecordServiceImpl.java
+++ VideoProject_BackEnd/src/main/java/com/backend/videoproject_backend/service/impl/RecordServiceImpl.java
@@ -1,18 +1,22 @@
 package com.backend.videoproject_backend.service.impl;
 
 import com.backend.videoproject_backend.dao.RecordDao;
+import com.backend.videoproject_backend.dao.UserRecordDao;
 import com.backend.videoproject_backend.dto.TbRecordEntity;
+import com.backend.videoproject_backend.dto.VUserRecordEntity;
 import com.backend.videoproject_backend.service.RecordService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class RecordServiceImpl implements RecordService {
     @Autowired
     private RecordDao recordDao;
+
+    @Autowired
+    private UserRecordDao userRecordDao;
 
     @Override
     public void addRecord(TbRecordEntity tbRecordEntity)
@@ -26,5 +30,9 @@ public class RecordServiceImpl implements RecordService {
         return recordDao.findByUserId(userId);
     }
 
+    @Override
+    public List<VUserRecordEntity> findUserRecordDesc() {
+        return userRecordDao.findByOrderBySumDistanceDesc();
+    }
 
 }
