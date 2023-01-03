ffe778591e65e3f8e6c348ff9c774b4ba01b6dbc
MQN-80 <3218290253@qq.com>
2023-01-03 18:15:30 +0800

    11

--- VideoProject_BackEnd/src/main/java/com/backend/videoproject_backend/controller/IndexController.java
+++ VideoProject_BackEnd/src/main/java/com/backend/videoproject_backend/controller/IndexController.java
@@ -27,20 +27,9 @@ public class IndexController {
     @ResponseBody
     @RequestMapping(value = "/sendMessage",method = RequestMethod.POST)
     public String sendMessage(@RequestParam String phone){
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-        System.out.println(phone);
-=======
         if(userService.findUserByPhone(phone).isPresent()){
             return "手机号已注册";
         }
->>>>>>> fdf969fdede295c9057c69a60e9f7d412c7add2e
-=======
-        if(userService.findUserByPhone(phone).isPresent()){
-            return "手机号已注册";
-        }
->>>>>>> fdf969fdede295c9057c69a60e9f7d412c7add2e
         //生成六位数随机验证码
         String code = RandomUtil.getSixBitRandom();
         //设置redis的key为用户手机号
