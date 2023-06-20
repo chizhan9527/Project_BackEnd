62703801e2db4809b513a72029c48f597623d01f
chizhan9527 <1346885811@qq.com>
2023-06-20 18:47:33 +0800

    单元测试

---
+++ RunWeTest/unit_testing/ArticleUseCase.py
@@ -0,0 +1,30 @@
+ids = ['1000000000', '114514', '2', '3', '-1']
+
+likes = [
+    {
+        "user_id": "1000000000",
+        "id": "2"
+    },
+    {
+        "user_id": "30",
+        "id": "2"
+    },
+    {
+        "user_id": "4",
+        "id": "1000000000"
+    },
+    {
+        "user_id": "4",
+        "id": "34"
+    },
+    {
+        "user_id": "4",
+        "id": "3"
+    },
+    {
+        "user_id": "4",
+        "id": "3"
+    },
+]
+
+contexts = [None, "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "hello,world!"]
---
+++ RunWeTest/unit_testing/ManagerUseCase.py
@@ -0,0 +1,273 @@
+changeRank = [
+    {
+        "as_id": "25",
+        "user_id": "16",
+        "manager_id": "16"
+    },
+    {
+        "as_id": "25",
+        "user_id": "4",
+        "manager_id": "4"
+    },
+    {
+        "as_id": "25",
+        "user_id": "14",
+        "manager_id": "14"
+    },
+    {
+        "as_id": "25",
+        "user_id": "20",
+        "manager_id": "21"
+    },
+    {
+        "as_id": "60",
+        "user_id": "16",
+        "manager_id": "4"
+    },
+    {
+        "as_id": "60",
+        "user_id": "4",
+        "manager_id": "16"
+    },
+    {
+        "as_id": "60",
+        "user_id": "14",
+        "manager_id": "20"
+    },
+    {
+        "as_id": "60",
+        "user_id": "20",
+        "manager_id": "14"
+    },
+    {
+        "as_id": "25",
+        "user_id": "15",
+        "manager_id": "14"
+    },
+    {
+        "as_id": "25",
+        "user_id": "4",
+        "manager_id": "20"
+    },
+    {
+        "as_id": "25",
+        "user_id": "14",
+        "manager_id": "15"
+    },
+    {
+        "as_id": "25",
+        "user_id": "20",
+        "manager_id": "4"
+    },
+    {
+        "as_id": "60",
+        "user_id": "15",
+        "manager_id": "20"
+    },
+    {
+        "as_id": "60",
+        "user_id": "4",
+        "manager_id": "14"
+    },
+    {
+        "as_id": "60",
+        "user_id": "14",
+        "manager_id": "4"
+    },
+    {
+        "as_id": "60",
+        "user_id": "20",
+        "manager_id": "15"
+    },
+]
+
+getClub = [
+    {
+        "user_id": "-1"
+    },
+    {
+        "user_id": "4"
+    },
+    {
+        "user_id": "18"
+    },
+    {
+        "user_id": "100000000"
+    },
+]
+
+getRank = [
+    {
+        "as_id": "25",
+        "user_id": "14"
+    },
+    {
+        "as_id": "26",
+        "user_id": "4"
+    },
+    {
+        "as_id": "1234",
+        "user_id": "123"
+    },
+    {
+        "as_id": "-1",
+        "user_id": "14"
+    },
+    {
+        "as_id": "100000000",
+        "user_id": "10"
+    },
+    {
+        "as_id": "25",
+        "user_id": "-1"
+    },
+    {
+        "as_id": "1234",
+        "user_id": "100000000"
+    },
+]
+
+postManager = [
+    {
+        "as_id": "25",
+        "user_id": "14"
+    },
+    {
+        "as_id": "26",
+        "user_id": "4"
+    },
+    {
+        "as_id": "1234",
+        "user_id": "123"
+    },
+    {
+        "as_id": "-1",
+        "user_id": "14"
+    },
+    {
+        "as_id": "100000000",
+        "user_id": "10"
+    },
+    {
+        "as_id": "25",
+        "user_id": "-1"
+    },
+    {
+        "as_id": "1234",
+        "user_id": "100000000"
+    },
+]
+
+deleteManager = [
+    {
+        "as_id": "25",
+        "user_id": "16",
+        "manager_id": "16"
+    },
+    {
+        "as_id": "25",
+        "user_id": "4",
+        "manager_id": "4"
+    },
+    {
+        "as_id": "25",
+        "user_id": "14",
+        "manager_id": "14"
+    },
+    {
+        "as_id": "25",
+        "user_id": "20",
+        "manager_id": "21"
+    },
+    {
+        "as_id": "60",
+        "user_id": "16",
+        "manager_id": "4"
+    },
+    {
+        "as_id": "60",
+        "user_id": "4",
+        "manager_id": "16"
+    },
+    {
+        "as_id": "60",
+        "user_id": "14",
+        "manager_id": "20"
+    },
+    {
+        "as_id": "60",
+        "user_id": "20",
+        "manager_id": "14"
+    },
+    {
+        "as_id": "25",
+        "user_id": "15",
+        "manager_id": "14"
+    },
+    {
+        "as_id": "25",
+        "user_id": "4",
+        "manager_id": "20"
+    },
+    {
+        "as_id": "25",
+        "user_id": "14",
+        "manager_id": "15"
+    },
+    {
+        "as_id": "25",
+        "user_id": "20",
+        "manager_id": "4"
+    },
+    {
+        "as_id": "60",
+        "user_id": "15",
+        "manager_id": "20"
+    },
+    {
+        "as_id": "60",
+        "user_id": "4",
+        "manager_id": "14"
+    },
+    {
+        "as_id": "60",
+        "user_id": "14",
+        "manager_id": "4"
+    },
+    {
+        "as_id": "60",
+        "user_id": "20",
+        "manager_id": "15"
+    },
+]
+
+getManager = [
+    {
+        "as_id": "26",
+        "rank": "0"
+    },
+    {
+        "as_id": "133333",
+        "rank": "1"
+    },
+    {
+        "as_id": "23",
+        "rank": "2"
+    },
+    {
+        "as_id": "25",
+        "rank": "-1"
+    },
+    {
+        "as_id": "26",
+        "rank": "3"
+    },
+    {
+        "as_id": "-1",
+        "rank": "0"
+    },
+    {
+        "as_id": "100000000",
+        "rank": "2"
+    },
+]
